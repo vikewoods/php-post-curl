af0dfd85e5f8efb5e3984ac86f443b83bcb6345a
Vik Ewoods <support@voroninstudio.eu>
2013-01-31 00:03:24 +0200

    Update uob.php

--- uob.php
+++ uob.php
@@ -6,19 +6,20 @@ $bank_code = "7199";
 $branch_code = "003";
 $bank_reference = "UOB-1242-X93";
 
+echo "\n\n\n";
 
 echo "Connnecting to UOB Server(202.42.213.5)...\n";
 echo "\n\n\n\n";
-echo "Generating user salt(sha1): ".sha1(md5(rand(100, 89805980)))."\n\n";
+echo "Generating user salt(sha1): ".sha1(md5(rand(100, 89805980)))."\n\n\n";
 sleep(15);
-echo "Welcome to UOB Terminal eBanking system\n";
-echo "THIS IS ONLY FOR AUTHORIZE INTERNAL BANK(UOB) USAGE AND BANK(UOB) MANAGERS USAGE\n";
+echo "Welcome to UOB Terminal eBanking system\n\n\n";
+echo "THIS IS ONLY FOR AUTHORIZE INTERNAL BANK(UOB) USAGE AND BANK(UOB) MANAGERS USAGE\n\n\n";
 echo "Legacy notice: \n\n";
 echo "Use Of Information And Materials\n\n";
 echo "The contents of this website and/or made available through the Services are provided for general information only and should not be used as a basis for making any specific investment, business or commercial decision. You should at all times consult your professional advisers and obtain independent verification of such information and data before making any decision based thereon.\n\n";
 echo "\n\n";
 sleep(5);
-echo "Deemed Permission\n";
+echo "Deemed Permission\n\n";
 echo "By using or accessing this website and/or the Services, you are deemed to have given permission to the UOB Group to disclose to anyone information relating to yourself, including customer information, in connection with any of your accounts with any member of the UOB Group as UOB shall deem fit for any purpose without incurring any liability of whatsoever nature.";
 echo "SSL Key to logon: ".sha1(md5(rand(100, 89805980)))."\n\n";
 echo "====================================================================\n";
@@ -36,9 +37,12 @@ echo "\n\n";
 echo "Bank response:";
 echo "\n";
 echo "====================================================================\n";
-echo "         ACCOUNT HAS BEEN CLOSED BY LOCAL AUTHORITY                 \n";
+echo "ACCOUNT ON HOLD BY BANK AND VALID AND WILL BE CLOSED ON 17 March 2013\n";
 echo "====================================================================\n";
 echo "\n";
 echo "\n";
+echo "====================================================================\n";
+echo "Closing connection \n\n\n".sha1(md5(rand(100, 89805980)))."\n\n";;
+
 
 ?>
